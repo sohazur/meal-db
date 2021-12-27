8492bdf5fee29defa7bb60024b3ccf96fda7acbe
Sohazur <mdsohazurislam@gmail.com>
2021-12-28 02:15:33 +0400

    first commit

---
+++ js/mealdb.js
@@ -0,0 +1,9 @@
+const searchFood = () => {
+  const searchField = document.getElementById("search-field");
+  const searchText = searchField.value;
+  searchField.value = "";
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+};
